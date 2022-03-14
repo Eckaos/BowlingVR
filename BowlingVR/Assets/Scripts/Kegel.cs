504c3a835dcfa069f0953251724e1a6f0b107e1f
Eckaos <nathan.pollart@hotmail.fr>
2022-03-14 17:14:48 -0400

    Update Kegel.cs

--- BowlingVR/Assets/Scripts/Kegel.cs
+++ BowlingVR/Assets/Scripts/Kegel.cs
@@ -7,6 +7,7 @@ public class Kegel : MonoBehaviour
     public GameObject point;
     public bool hasFallen () 
     {  
+        Debug.Log(point.transform.localPosition.y +"   "+ GetComponent<Renderer>().bounds.size.y/2);
         return point.transform.localPosition.y < GetComponent<Renderer>().bounds.size.y/2;
     }
 }
