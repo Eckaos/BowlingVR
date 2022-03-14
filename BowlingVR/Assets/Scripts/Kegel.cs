2c80e76d321318123934190e46eb62bb9f0158a3
Eckaos <nathan.pollart@hotmail.fr>
2022-03-14 17:41:17 -0400

    Update Kegel.cs

--- BowlingVR/Assets/Scripts/Kegel.cs
+++ BowlingVR/Assets/Scripts/Kegel.cs
@@ -7,6 +7,6 @@ public class Kegel : MonoBehaviour
     public GameObject point;
     public bool hasFallen () 
     {  
-        return point.transform.localEulerAngles.z  > 30 || point.transform.localEulerAngles.z < -30 || point.transform.localEulerAngles.x > 30 || point.transform.localEulerAngles.x < -30;
+        return Vector3.Angle(transform.up, Vector3.up) > 5;
     }
 }
