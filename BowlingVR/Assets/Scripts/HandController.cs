dfcbc82561ff05d3bb04d5b657f727b1f66cab0d
Eckaos <nathan.pollart@hotmail.fr>
2022-03-21 17:42:49 -0400

    HandController #1  by @Suna24

--- BowlingVR/Assets/Scripts/HandController.cs
+++ BowlingVR/Assets/Scripts/HandController.cs
@@ -1,11 +1,12 @@
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine;
 
+[RequireComponent(typeof(ActionBasedController))]
 public class HandController : MonoBehaviour
 {
 
     ActionBasedController controller;
-    Hand hand;
+    public Hand hand;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,6 @@ public class HandController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        hand.SetGrip(controller.selectAction.action.ReadValue<float>);
     }
 }
