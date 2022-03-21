be24a582b42a31150e7c42e56a6b01a15290f726
Eckaos <nathan.pollart@hotmail.fr>
2022-03-21 16:36:21 -0400

    ScoreTable logic

---
+++ BowlingVR/Assets/Scripts/ScoreTable.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTable : MonoBehaviour
+{
+    [SerializeField]private List<Text> scoreTexts;
+
+    public void SetScores(List<int> scores)
+    {
+        int index = 0;
+        foreach (var score in scores)
+        {
+            if(score == 10)
+                scoreTexts[++index].text = "X";
+            else if(score+int.Parse(scoreTexts[index-1].text) == 10)
+                scoreTexts[index].text = "/";
+            else if(score == 0)
+                scoreTexts[index].text = "-";
+            else
+                scoreTexts[index].text = score.ToString();
+            
+            index++;
+        }
+    }
+}
